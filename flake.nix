c0460f0b7ae567c9b3c64ecae5ec96b012570d85
Clemens Boos <clemensboos@gmail.com>
2020-08-17 02:09:27 +0200

    Move models to omnetppModels, add app

--- flake.nix
+++ flake.nix
@@ -31,67 +31,76 @@
                 withSWIG = false;
               };
 
-              example-project = final.omnetpp.buildModel {
-                pname = "example-project";
-                version = "0.0.1";
+              omnetppModels = final.lib.makeOverridable ({ omnetpp }: {
+                example-project = omnetpp.buildModel {
+                  pname = "example-project";
+                  version = "0.0.1";
 
-                src = "${self}/example-project";
-              };
-
-              inet = final.omnetpp.buildModel {
-                pname = "inet";
-                version = "4.2.0";
-
-                src = final.fetchFromGitHub {
-                  owner = "inet-framework";
-                  repo = "inet";
-                  rev = "cb6c37b3dcb76b0cecf584e87e777d965bf1ca6c";
-                  sha256 = "sha256-oxCz5Dwx5/NeINPAaXmx6Ie/gcMLu9pmVb2A35e0C6s=";
+                  src = "${self}/example-project";
                 };
 
-                extraIncludeDirs = [ "src" ];
-              };
+                inet = omnetpp.buildModel {
+                  pname = "inet";
+                  version = "4.2.0";
 
-              veins = final.omnetpp.buildModel {
-                pname = "veins";
-                version = "5.0-git";
+                  src = final.fetchFromGitHub {
+                    owner = "inet-framework";
+                    repo = "inet";
+                    rev = "cb6c37b3dcb76b0cecf584e87e777d965bf1ca6c";
+                    sha256 = "sha256-oxCz5Dwx5/NeINPAaXmx6Ie/gcMLu9pmVb2A35e0C6s=";
+                  };
 
-                src = final.fetchFromGitHub {
-                  owner = "ClemensB";
-                  repo = "veins";
-                  rev = "e9fcb936f17f9938be8d8f46be2d086f05744bf8";
-                  sha256 = "sha256-pAJ9IiKry+uwVz3UizzJ2fzhmucU7b2Gm/RtdvHc4oI=";
+                  extraIncludeDirs = [ "src" ];
                 };
-              };
 
-              veins_inet = final.omnetpp.buildModel {
-                pname = "veins_inet";
-                version = "4.0-git";
+                veins = omnetpp.buildModel {
+                  pname = "veins";
+                  version = "5.0-git";
 
-                src = final.veins.src;
-                sourceRoot = "source/subprojects/veins_inet";
+                  src = final.fetchFromGitHub {
+                    owner = "ClemensB";
+                    repo = "veins";
+                    rev = "e9fcb936f17f9938be8d8f46be2d086f05744bf8";
+                    sha256 = "sha256-pAJ9IiKry+uwVz3UizzJ2fzhmucU7b2Gm/RtdvHc4oI=";
+                  };
+                };
 
-                buildInputs = with final; [
-                  inet
-                  veins
-                ];
-              };
+                veins_inet = omnetpp.buildModel {
+                  pname = "veins_inet";
+                  version = "4.0-git";
+
+                  src = final.veins.src;
+                  sourceRoot = "source/subprojects/veins_inet";
+
+                  buildInputs = with final; [
+                    inet
+                    veins
+                  ];
+                };
+              }) { omnetpp = final.omnetpp; };
             };
 
         packages.x86_64-linux = {
           inherit (pkgs)
-            osgearth
-
             omnetpp
-
+            osgearth
             sumo
-            sumo-minimal
+            sumo-minimal;
 
+          inherit (pkgs.omnetppModels)
+            example-project
             inet
             veins
-            veins_inet
-
-            example-project;
+            veins_inet;
         };
+
+        apps.x86_64-linux = {
+          omnetpp = {
+            type = "app";
+            program = "${self.packages.x86_64-linux.omnetpp.ide}/bin/omnetpp";
+          };
+        };
+
+        defaultApp.x86_64-linux = self.apps.x86_64-linux.omnetpp;
       };
 }
